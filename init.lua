9a8b702c92baeb52e4ec19efe9f264af2607e824
Ivar Fatland <ivarfatland@pop-os.localdomain>
2023-05-28 15:45:23 +0200

    Supressed undefined global vim

--- init.lua
+++ init.lua
@@ -144,9 +144,22 @@ local function packer_startup(use)
     }
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     require("mason-lspconfig").setup_handlers {
-        function (server_name)
+        function (server_name) -- default_handler
             require("lspconfig")[server_name].setup {
                 capabilities = capabilities
+            }
+        end,
+
+        lua_ls = function()
+            require("lspconfig").lua_ls.setup {
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = {'vim'}
+                        }
+                    }
+                }
             }
         end,
     }
