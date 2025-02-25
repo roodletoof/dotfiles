2127e6a82932e50438bea8f379a36e0e76930b52
Ivar Fatland <ivar.fatland@bouvet.no>
2025-02-25 09:44:59 +0100

    fixed stupid mkview/loadview default, switched from oil to nvim-tree

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
 ---@diagnostic disable: missing-fields
 -- vim:foldmethod=marker
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 local function get_python_venv_path() --{{{1
     return vim.fn.stdpath('config') .. '/.venv/bin/python'
 end
@@ -47,6 +50,8 @@ vim.cmd [[
     autocmd BufEnter * if &buftype == 'terminal' | :startinsert | endif 
     nnoremap ,t <c-w>v<c-w>l:terminal<CR>a
 
+    " Don't include curdir, it just causes pain.
+    set viewoptions=folds,cursor
     autocmd BufWinLeave *.* silent! mkview 
     autocmd BufWinEnter *.* silent! loadview 
 
@@ -124,6 +129,19 @@ require'lazy'.setup{ --{{{1
             require'mini.align'.setup()
         end,
     },
+    { 'nvim-tree/nvim-tree.lua', --{{{2
+        dependencies = {"nvim-tree/nvim-web-devicons"},
+        config = function ()
+            require'nvim-tree'.setup{
+                update_focused_file = {
+                    enable = true,
+                    update_root = {
+                        enable = true
+                    }
+                }
+            }
+        end
+    },
     { 'sainnhe/everforest', --{{{2
         lazy = false,
         priority = 1000,
@@ -143,28 +161,6 @@ require'lazy'.setup{ --{{{1
                 { silent = true }
             )
         end
-    },
-    { 'stevearc/oil.nvim', --{{{2
-        dependencies = { 'nvim-tree/nvim-web-devicons', },
-        config = function ()
-            local oil = require('oil')
-            oil.setup{
-                default_file_explorer = true,
-                columns = {'icon'},
-                view_options = {
-                    show_hidden = true,
-                },
-                lsp_file_methods = {
-                    enables = true,
-                    timeout_ms = 1000,
-                    autosave_changes = true,
-                },
-                keymaps = {
-                    [",cd"] = { "actions.cd", mode = "n" },
-                },
-            }
-            vim.keymap.set("n", "-", vim.cmd.Oil, { desc = "Open parent directory" })
-        end,
     },
     { 'seblyng/roslyn.nvim', --{{{2
         --WARN: requires html-lsp, roslyn and rzls installed via Mason
