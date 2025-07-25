1acc962d4e4cc12206cec7eac4e6c3a223823f8b
Ivar Fatland <fatland99@hotmail.com>
2025-07-25 21:25:46 +0200

    disable retarted treesitter indenting rules. they suck. treesitter sucks ass

--- init.lua
+++ init.lua
@@ -518,7 +518,7 @@ require'lazy'.setup{ --{{{1
                 sync_install = false,
                 auto_install = true,
                 indent = {
-                    enable = true,
+                    enable = false,
                 },
                 highlight = {
                     enable = true,
