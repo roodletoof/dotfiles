4a70762e65330c65369a36f954bcd6df7a8dae92
Ivar Fatland <fatland99@hotmail.com>
2025-07-27 23:08:33 +0200

    change some zen mode options and set width to flat 90.
    set to 90 because line numbers add soe width, and I want some margin if
    the number of lines are large in files

--- init.lua
+++ init.lua
@@ -373,11 +373,9 @@ require'lazy'.setup{ --{{{1
                 function()
                     zen_mode.toggle{
                         window = {
-                            width = 80,
+                            width = 90,
                             options = {
                                 signcolumn = "no", -- disable signcolumn
-                                number = false, -- disable number column
-                                relativenumber = false, -- disable relative numbers
                                 cursorline = false, -- disable cursorline
                                 cursorcolumn = false, -- disable cursor column
                                 foldcolumn = "0", -- disable fold column
