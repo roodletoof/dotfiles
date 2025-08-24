75aa4f74cfc7483015f07ed0542fe6a84e481a37
Ivar Fatland <fatland99@hotmail.com>
2025-08-25 00:46:52 +0200

    add battery saving options

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -53,6 +53,16 @@
             };
         };
     };
+    services.tlp = {
+        enable = true;
+        settings = {
+            CPU_BOOST_ON_BAT = 0;
+            CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
+            START_CHARGE_THRESH_BAT0 = 90;
+            STOP_CHARGE_THRESH_BAT0 = 97;
+            RUNTIME_PM_ON_BAT = "auto";
+        };
+    };
 
 	environment.systemPackages = with pkgs; [
         grim
