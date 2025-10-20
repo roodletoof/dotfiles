f1e3ecfd0138fc0b8fc21ff21e336b11fe0d5252
Ivar Fatland <ivar.fatland@akerbp.com>
2025-10-20 09:47:20 +0200

    update injections and snippets

--- nvim/.config/nvim/queries/yaml/injections.scm
+++ nvim/.config/nvim/queries/yaml/injections.scm
@@ -7,6 +7,14 @@
     (#offset! @injection.content 0 1 0 0)))
 
 (block_mapping_pair
+  key: (flow_node) @_run
+  (#any-of? @_run "query")
+  value: (flow_node
+    (double_quote_scalar) @injection.content
+    (#set! injection.language "sql")
+    (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
   key: (flow_node (plain_scalar (string_scalar) @injection.language)) 
   value: (block_node
     (block_scalar) @injection.content
