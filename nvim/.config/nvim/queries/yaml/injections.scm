1be3a2a238183a9fa9723717bb95dd6558af23d0
Ivar Fatland <ivar.fatland@akerbp.com>
2025-10-30 10:27:48 +0100

    add markdown injection in yaml for documentation block nodes

--- nvim/.config/nvim/queries/yaml/injections.scm
+++ nvim/.config/nvim/queries/yaml/injections.scm
@@ -8,6 +8,14 @@
 
 (block_mapping_pair
   key: (flow_node) @_run
+  (#any-of? @_run "documentation")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#set! injection.language "markdown")
+    (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_run
   (#any-of? @_run "query")
   value: (flow_node
     (double_quote_scalar) @injection.content
