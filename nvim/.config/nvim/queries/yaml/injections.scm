645fbc2b0b85d35014673ea663482e5cbb5d295e
Ivar Fatland <ivar.fatland@akerbp.com>
2025-11-24 09:02:13 +0100

    highlight script fields as bash

--- nvim/.config/nvim/queries/yaml/injections.scm
+++ nvim/.config/nvim/queries/yaml/injections.scm
@@ -8,6 +8,14 @@
 
 (block_mapping_pair
   key: (flow_node) @_run
+  (#any-of? @_run "script")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#set! injection.language "bash")
+    (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_run
   (#any-of? @_run "documentation")
   value: (block_node
     (block_scalar) @injection.content
