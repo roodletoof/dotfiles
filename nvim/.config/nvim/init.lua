7eb25b8b34ac269a2c08f557b16a04aa8b67eece
Ivar Fatland <fatland99@hotmail.com>
2026-02-04 19:49:31 +0100

    keybind for make

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -45,6 +45,7 @@ vim.cmd [[
     nnoremap ,a <C-a>
     nnoremap ,x <C-x>
     nnoremap ,rl :checktime<CR>
+    nnoremap <c-m> :make<CR>
 
     nnoremap ,cD :call setqflist(filter(getqflist(), 'v:val != getqflist()[getqflist({"idx": 0}).idx - 1]'))<CR>
 
