5a4abe74547887021d28e0793578528766c1b6bc
Ivar Fatland <ivar.fatland@akerbp.com>
2026-03-04 09:45:23 +0100

    remove unused oil keybindings and unbind my movement keys

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -387,21 +387,14 @@ require'lazy'.setup{ --{{{1
                 keymaps = {
                     ['g?'] = { 'actions.show_help', mode = 'n' },
                     ['<C-y>'] = { 'actions.yank_entry', opts = { modify=":." }, mode = 'n' }, -- :. makes it a relative path
+                    ['<C-h>'] = false, ['<C-j>'] = false, ['<C-k>'] = false, ['<C-l>'] = false, -- I use those keys for navigation
                     ['<CR>'] =  'actions.select',
-                    ['<C-v>'] = { 'actions.select', opts = { vertical = true } },
-                    ['<C-s>'] = { 'actions.select', opts = { horizontal = true } },
-                    ['<C-t>'] = { 'actions.select', opts = { tab = true } },
-                    ['<C-p>'] = 'actions.preview',
-                    ['<C-c>'] = { 'actions.close', mode = 'n' },
-                    ['<C-l>'] = 'actions.refresh',
                     ['-'] = { 'actions.parent', mode = 'n' },
                     ['_'] = { 'actions.open_cwd', mode = 'n' },
                     [',cd'] = { 'actions.cd', mode = 'n' },
                     [',CD'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
-                    ['gs'] = { 'actions.change_sort', mode = 'n' },
                     ['gx'] = 'actions.open_external',
                     ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
-                    ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
                 },
                 use_default_keymaps = true,
                 view_options = {
