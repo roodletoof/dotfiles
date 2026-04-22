35f3e0048096e409ff4af0bc89060cc3b2c1309e
Ivar Fatland <ivar.fatland@akerbp.com>
2026-04-22 12:18:35 +0200

    append instaed of prepend

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -81,11 +81,11 @@ vim.cmd [=[
     set errorformat+=%f:%l:%c:\ %t%*[^:]:\ %m
 
     set wildignore=*.o,*.obj,.git/**,tags,*.pyc
-    set errorformat^=[----]\ %f:%l:\ %m
-    set errorformat^=%f\(%l:%c\)\ %m
+    set errorformat+=[----]\ %f:%l:\ %m
+    set errorformat+=%f\(%l:%c\)\ %m
 
-    set errorformat^=%C%*\\s——▶%*\\s%f:%l:%c
-    set errorformat^=%E%t%*[^:]:\ %m
+    set errorformat+=%E%t%*[^:]:\ %m
+    set errorformat+=%C%*\\s——▶%*\\s%f:%l:%c
 ]=]
 
 vim.keymap.set('n', ',cf', function()
