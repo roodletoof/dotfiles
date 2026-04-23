ac0e9c3c74dbf78131672b8a310fd34295d02944
Ivar Fatland <ivar.fatland@akerbp.com>
2026-04-23 16:45:11 +0200

    force cs parser
    temporary fix. make better solution when at home

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -220,7 +220,10 @@ vim.api.nvim_create_autocmd({
     end
 })
 
--- FILE SPECIFIC AND AUTOCMDS {{{1
+--- tree-sitter and file specific {{{1
+
+assert(vim.treesitter.language.add('c_sharp'))
+vim.treesitter.language.register('c_sharp', 'cs')
 local file_specific = {
     c = function()
         vim.bo.expandtab = false
@@ -231,7 +234,7 @@ local file_specific = {
     end,
     odin = function()
         vim.bo.expandtab = false
-    end
+    end,
 }
 
 vim.api.nvim_create_autocmd('BufEnter', {
