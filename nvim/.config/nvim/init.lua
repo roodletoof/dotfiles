ddfd79d32f407f7e90b6cdb2b630cb764102dca1
Ivar Fatland <ivarfatland@Ivars-Air.lan>
2026-04-26 22:47:07 +0200

    slight improvement in error format... kinda...

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -142,11 +142,13 @@ vim.cmd [=[
     set errorformat+=[----]\ %f:%l:\ %m
     set errorformat+=%f\(%l:%c\)\ %m
 
-    set errorformat+=%E%t%*[^:]:\ %m
-    set errorformat+=%C%*\\s——▶%*\\s%f:%l:%c
+    set errorformat+=%E%t%*[^:]:\ %m,Z%*\\s——▶%*\\s%f:%l:%c
 
     set errorformat+=%f:%l:%c\ -\ %t%*[^:]:\ %m
     set errorformat+=%f:%l:\ %m
+    " rust
+    set errorformat+=%E%tarning:%m,%Z\ -->\ %f:%l:%c
+    set errorformat+=%E%terror:%m,%Z\ -->\ %f:%l:%c
 
 ]=]
 
@@ -165,6 +167,7 @@ vim.keymap.set('n', ',cl', function()
     local qf = vim.fn.getqflist()
     for i = #qf, 1, -1 do
         local item = qf[i]
+        
         if item.valid == 1 then
             vim.cmd('cc '..i)
             return
