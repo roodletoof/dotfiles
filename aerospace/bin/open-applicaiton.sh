a2f05963bbe9d7a9617e9b87022919308d06f460
Ivar Fatland <ivarfatland@Ivars-Air.lan>
2026-05-10 01:39:06 +0200

    use $HOME variable

--- aerospace/bin/open-applicaiton.sh
+++ aerospace/bin/open-applicaiton.sh
@@ -1,4 +1,4 @@
-ls /Applications/ /Applications/Utilities/ /System/Applications/ /System/Applications/Utilities/ /Users/ivar.fatland/Applications/ /Users/ivar.fatland/Applications/Chrome\ Apps.localized/ | \
+ls /Applications/ /Applications/Utilities/ /System/Applications/ /System/Applications/Utilities/ $HOME/Applications/ $HOME/Applications/Chrome\ Apps.localized/ | \
     grep '\.app$' | \
     sed 's/\.app$//g' | \
     { cat; echo "Finder"; } | \
