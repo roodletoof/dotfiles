e57750c2750e7b5cff1aa744d238067aa96dc344
Ivar Fatland <ivarfatland@Ivars-Air.lan>
2026-05-09 22:56:46 +0200

    Revert "remove finder"
    This reverts commit 4e714896164e5c7fcc63c2c77592fd7aaeb4abec.

--- aerospace/bin/open-applicaiton.sh
+++ aerospace/bin/open-applicaiton.sh
@@ -1,6 +1,7 @@
 ls /Applications/ /Applications/Utilities/ /System/Applications/ /System/Applications/Utilities/ /Users/ivar.fatland/Applications/ /Users/ivar.fatland/Applications/Chrome\ Apps.localized/ | \
     grep '\.app$' | \
     sed 's/\.app$//g' | \
+    { cat; echo "Finder"; } | \
     sort | \
     raymenu | \
     xargs -I {} open -n -a "{}.app"
