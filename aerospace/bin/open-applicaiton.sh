0d6e69d682c03aa392b8bafa048ae20c83ddd155
Ivar Fatland <ivarfatland@Ivars-Air.lan>
2026-05-09 23:04:54 +0200

    patch for finder

--- aerospace/bin/open-applicaiton.sh
+++ aerospace/bin/open-applicaiton.sh
@@ -4,4 +4,13 @@ ls /Applications/ /Applications/Utilities/ /System/Applications/ /System/Applica
     { cat; echo "Finder"; } | \
     sort | \
     raymenu | \
-    xargs -I {} open -n -a "{}.app"
+    while read app; do
+        if [ "$app" = "Finder" ]; then
+            osascript -e 'tell application "Finder"
+                activate
+                make new Finder window
+            end tell'
+        else
+            open -n -a "$app.app"
+        fi
+    done
